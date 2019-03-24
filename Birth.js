0ee647fb92eab72f1a26eda4d114e56b67a0c8ec
devil87119 <devil87119@yahoo.com.tw>
2019-03-24 22:50:46 +0800

    check ur age

---
+++ Birth.js
@@ -0,0 +1,40 @@
+
+function start() {
+	var dead = "1900/1/2";
+	var t = 0;
+    var birth = prompt("请输入您的生日","1900/1/1");	
+	while (isNaN(Date.parse(birth))||Date.parse(birth).valueOf()<Date.parse(dead).valueOf()) {
+		if(Date.parse(birth).valueOf()<Date.parse(dead).valueOf()){
+			if(t<2)alert("唬爛啦!");
+		}
+		else {
+			if(t<2)alert("日期格式錯誤!");
+		}
+		if(t>2&&t<4){
+			alert("屁孩掰掰!");
+		}
+		if(t>2){window.location.href='https://4thminato.github.io/Naruto/';}
+		if(t<2){var birth = prompt("请输入您的生日","1900/1/1");}
+		t++;
+	} 
+	var year = 1000 * 60 * 60 * 24 * 365;
+	var age = parseInt((now - birthday) / year);
+	var now = new Date();
+	var birthday = new Date(birth);
+	var age = parseInt((now - birthday) / year);
+	if(age<18){
+		alert("警告!!!你小於18歲!乖乖玩遊戲吧~~");
+		window.location.href='https://4thminato.github.io/Naruto/';
+	}
+	else {
+		alert("你"+age+"歲了!");
+	}
+}
+
+window.addEventListener( "load", start, false );
+/*$(document).ready(function() {
+
+
+$(“table tr:nth-child(even)”).addClass(“even”); 
+});
+*/
